90dd2781e3e077fc27293ff4b5c481f6dc25781e
elonmusk64 <amey.surwase55@gmail.com>
2025-07-27 16:22:23 +0530

    Add .next/static/chunks/src_app_profile_page_tsx_11138ccc._.js 1753613526124

---
+++ .next/static/chunks/src_app_profile_page_tsx_11138ccc._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_dc4356b3._.js",
+  "static/chunks/src_79cc5e96._.js"
+],
+    source: "dynamic"
+});
